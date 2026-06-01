df8ec7905dc27cf7a9375eedf422ff89afd8734d
Vinay patel <vinu151718@gmail.com>
2026-06-01 11:04:18 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://vinu151718_db_user:<Vinu00786>@cluster0.bdbziiq.mongodb.net/");
+mongoose.connect("mongodb+srv://vinu151718_db_user:12345@cluster0.bdbziiq.mongodb.net/");
 1
 const User = mongoose.model("User", {
   name: String
